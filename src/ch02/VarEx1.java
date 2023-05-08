9615cae800ab4d2b6d127324e8c652723ad38772
CHOIGOYO <choigoyo.q7@gmail.com>
2023-05-09 02:57:26 +0900

    변수에대한 학습내용 정리와 실습

---
+++ src/ch02/VarEx1.java
@@ -0,0 +1,13 @@
+package ch02;
+
+public class VarEx1 {
+    public static void main(String[] args) {
+//        6+3의 결과를 출력한다.
+        System.out.println("6 + 3 = " +(6+3));
+
+//        변수를 사용해서 연산결과를 출력한다.
+        int a = 10;
+        int b = 3;
+        System.out.println("a + b = "+(a+b));
+    }
+}
